965928ecc414a77292571284636c463cf072d956
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-21 15:05:15 -0400

    test

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,29 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import BubblePage from "./BubblePage";
 
-test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+function mockGetColors(colorData) {
+  return colorData;
+}
+
+const colorData = {
+  data: [
+    {
+      color: "aliceblue",
+      code: {
+        hex: "#f0f8ff",
+      },
+      id: 1,
+    },
+    {
+      color: "limegreen",
+      code: {
+        hex: "#99ddbc",
+      },
+      id: 2,
+    },
+  ],
+};
+
+jest.mock(mockGetColors());
+test("Fetches data and renders the bubbles", async () => {
+  expect(mockGetColors() === colorData);
 });
