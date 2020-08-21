47475eb40cf7205a64c45b8922d0bdd3c7fb0491
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-21 12:48:10 -0400

    Creace Login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import ExitToAppRoundedIcon from "@material-ui/icons/ExitToAppRounded";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+import { login } from "../network";
+
+const initialValues = {
+  username: "",
+  password: "",
 };
 
-export default Login;
+export function Login(props) {
+  const [values, setValues] = useState(initialValues);
+
+  function onChange(event) {
+    const { id, value } = event.target;
+
+    setValues({ ...values, [id]: value });
+  }
+
+  return (
+    <Box m={1}>
+      <Card variant="outlined">
+        <Typography variant="h2">Log In</Typography>
+        <Box m={1}>
+          <TextField
+            fullWidth
+            id="username"
+            label="Username"
+            onChange={onChange}
+            required
+            value={values.username}
+            variant="outlined"
+          />
+        </Box>
+        <br />
+        <Box m={1}>
+          <TextField
+            autoComplete="current-password"
+            fullWidth
+            id="password"
+            label="Password"
+            onChange={onChange}
+            required
+            type="password"
+            value={values.password}
+            variant="outlined"
+          />
+        </Box>
+        <Box m={1}>
+          <Button
+            disabled={
+              values.username.trim() === "" || values.password.trim() === ""
+            }
+            onClick={() => {
+              props.login(values.username, values.password);
+              setValues(initialValues);
+            }}
+            startIcon={<ExitToAppRoundedIcon />}
+            variant="contained"
+          >
+            Log In
+          </Button>
+        </Box>
+      </Card>
+    </Box>
+  );
+}
+export default connect(null, { login })(Login);
+
+//  LocalWords:  ExitToAppRounded
